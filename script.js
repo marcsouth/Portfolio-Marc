b7478e6439239877ddd3c43bfecf47ed908387c4
marcsouth <104800775+marcsouth@users.noreply.github.com>
2023-01-23 22:11:29 -0500

    yes

--- script.js
+++ script.js
@@ -65,3 +65,26 @@ $(function() {
       css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
       document.body.appendChild(css);
   };
+
+
+
+  // This is script for revealing elements //
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
